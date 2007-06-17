5d3e511fc59695f5937120f427355165ce9f8f65
james22 <james22>
2007-06-17 16:17:54 +0000

    modify start message for better display

--- src/de/ueller/gps/sirf/SirfInput.java
+++ src/de/ueller/gps/sirf/SirfInput.java
@@ -4,7 +4,7 @@
  * takes an InputStream and interpret layer 3 and layer 4. Than make
  * callbacks to the receiver witch ahas to implement SirfMsgReceiver 
  *
- * @version $Revision: 1.8 $$ ($Name:  $)
+ * @version $Revision: 1.9 $$ ($Name:  $)
  * @autor Harald Mueller james22 at users dot sourceforge dot net
  * Copyright (C) 2007 Harald Mueller
  */
@@ -44,7 +44,7 @@ public class SirfInput implements Runnable, LocationMsgProducer{
 	}
 
 	public void run(){
-		receiver.receiveMessage("start Tread");
+		receiver.receiveMessage("start SIRF receiver");
 		try {
 			while (ins.available() > 0)
 				ins.read();
