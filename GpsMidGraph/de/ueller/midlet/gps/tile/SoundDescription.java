94a7dd911ac1826b828cb107eed158c029813f4f
sk750 <sk750>
2008-07-21 17:38:52 +0000

    Read in sound file names from legend. This is a preparation for sounds in GpsMid.
    Map version is now 16.

---
+++ GpsMidGraph/de/ueller/midlet/gps/tile/SoundDescription.java
@@ -0,0 +1,22 @@
+/**
+ * GpsMid 
+ *  
+ *
+ * @version $Revision: 1.1 $ ($Name:  $)
+ *
+ * Copyright (C) 2007 Harald Mueller
+ */
+package de.ueller.midlet.gps.tile;
+
+/**
+ * @author sk750
+ *
+ */
+public class SoundDescription {
+	public String	name;
+	public String   soundFile;
+	
+	public String toString() {
+		return "Name: " + name + " SoundFile: " + soundFile;
+	}
+}
