8d989bd585389d8793888bb447d5cb83dfc7093d
Jan Rose <janrose.1986@googlemail.com>
2009-03-23 11:39:28 +0100

    changed build script

--- gpsmid_comp_test.sh
+++ gpsmid_comp_test.sh
@@ -5,5 +5,5 @@ cd ../Osm2GpsMid/
 ant deploy
 cp dist/Osm2GpsMid-0.4.55.jar /home/jan/Desktop/
 cd /home/jan/Desktop/
- java -Xmx1024M -jar Osm2GpsMid-0.4.55.jar schleswig-holstein.osm luebeck
+ java -Xmx1024M -jar Osm2GpsMid-0.4.55.jar germany.osm nordisch
 /usr/lib/jvm/WTK2.5.2/bin/emulator -Xdevice:DefaultColorPhone -Xdescriptor: ~/Desktop/GpsMid-Luebeck-0.4.55.jad
