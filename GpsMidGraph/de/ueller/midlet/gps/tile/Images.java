566661da2a959f5afc7b1f92ed6ad1f377bd40cb
Jan Rose <janrose.1986@googlemail.com>
2009-03-23 22:40:40 +0100

    Documenting the Image class

--- GpsMidGraph/de/ueller/midlet/gps/tile/Images.java
+++ GpsMidGraph/de/ueller/midlet/gps/tile/Images.java
@@ -4,8 +4,13 @@ import java.io.IOException;
 
 import javax.microedition.lcdui.Image;
 
+/**
+ * holds all the references to images and icons used in the programm
+ */
 public class Images {
+	/** used for currently recording gpx-tracks */
 	public final Image IMG_MARK = Image.createImage("/mark.png");
+	/** used for gpx-tracks loaded from the database */
 	public final Image IMG_MARK_DISP = Image.createImage("/mark_display.png");
 	/*public final Image IMG_PARKING=Image.createImage("/parking.png");
 	public final Image IMG_FUEL=Image.createImage("/fuel.png");
@@ -13,6 +18,7 @@ public class Images {
 	public final Image IMG_TELEPHONE=Image.createImage("/telephone.png");
 	public final Image IMG_RAILSTATION=Image.createImage("/railstation.png");
 	public final Image IMG_AERODROME=Image.createImage("/aerodrome.png");*/
+	/** icon for the routing target point */
 	public final Image IMG_TARGET=Image.createImage("/target.png");
 	public final Image IMG_STRAIGHTON=Image.createImage("/straighton.png");
 	public final Image IMG_LEFT=Image.createImage("/left.png");
