472369e1e49fb6ba101c6487350021c7de14b648
Jan Rose <janrose.1986@googlemail.com>
2009-03-23 13:36:27 +0100

    added undisplay loaded tracks & changed layers of tracks

--- gpsmid_comp_test.sh
+++ gpsmid_comp_test.sh
@@ -5,5 +5,5 @@ cd ../Osm2GpsMid/
 ant deploy
 cp dist/Osm2GpsMid-0.4.55.jar /home/jan/Desktop/
 cd /home/jan/Desktop/
- java -Xmx1024M -jar Osm2GpsMid-0.4.55.jar germany.osm nordisch
+ java -Xmx1024M -jar Osm2GpsMid-0.4.55.jar germany.osm luebeck
 /usr/lib/jvm/WTK2.5.2/bin/emulator -Xdevice:DefaultColorPhone -Xdescriptor: ~/Desktop/GpsMid-Luebeck-0.4.55.jad
